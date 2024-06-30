aa49c8cf924544cac775d05e53fcebd2852c0385
dallinmoak <dallinmoak@gmail.com>
2024-06-29 23:00:25 -0600

    add one get endpoint

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,13 @@ router.get("/", (req, res) => {
   res.send(`<h1>DM's Enclave!</h1>`);
 });
 router.use("/api-docs", api);
+router.get("/character/1", (req, res) => {
+  res.send({
+    name: "Gandalf the Grey",
+    class: "Wizard",
+    armorClass: 10,
+    type: "player",
+  });
+});
 
 export default router;
