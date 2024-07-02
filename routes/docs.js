9202bfaaeb8e07f855759426d66c2ba7270e23bd
DarthZ96 <mcc17067@byui.edu>
2024-07-02 11:00:21 -0600

    testing

--- routes/docs.js
+++ routes/docs.js
@@ -6,6 +6,7 @@ const require = createRequire(import.meta.url);
 const router = Router();
 
 router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(require("../swagger.json")));
+//router.get("/", swaggerUi.setup(require("../swagger.json")));
+router.get("/", swaggerUi.setup(require("../swagger-temp.yaml")));
 
 export default router;
