9bbc92d9f2a4f456b6a10d4fa84149e69214c0ca
dallinmoak <dallinmoak@gmail.com>
2024-07-02 20:04:43 -0600

    example ts express handler params syntax

--- routes/index.ts
+++ routes/index.ts
@@ -1,19 +1,23 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import api from "./docs";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+const welcomeMsg = (req: Request, res: Response) => {
   res.send(`<h1>DM's Enclave!</h1>`);
-});
-router.use("/api-docs", api);
-router.get("/character/1", (req, res) => {
+};
+
+const get1Character = (req: Request, res: Response) => {
   res.status(200).send({
     name: "Gandalf the Grey",
     class: "Wizard",
     armorClass: 10,
     type: "player",
   });
-});
+};
+
+router.get("/", welcomeMsg);
+router.use("/api-docs", api);
+router.get("/character/1", get1Character);
 
 export default router;
