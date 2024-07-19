0c9ed2583d59c5b9adc942ea6df472081ad61470
Zach McClure <mcc17067@byui.edu>
2024-07-18 21:26:02 -0600

    commenting out temporarily

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,63 +1,63 @@
-import { ObjectId } from "mongodb";
-import UserModel from "../models/user";
-import { Request, Response } from "express";
-import performSave from "../db/save";
-import { sendFailRes } from "./utils";
-import { Document } from "mongoose";
+// import { ObjectId } from "mongodb";
+// import UserModel from "../models/user";
+// import { Request, Response } from "express";
+// import performSave from "../db/save";
+// import { sendFailRes } from "./utils";
+// import { Document } from "mongoose";
 
-const getAll = async (req: Request, res: Response) => {
-  // const newUser = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const user = new UserModel(newUser);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(user, success, fail);
-};
+// const getAll = async (req: Request, res: Response) => {
+//   // const newUser = {
+//   //   ...req.body,
+//   //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
+//   // };
+//   // const user = new UserModel(newUser);
+//   // const success = (record: Document) => res.status(200).json(record);
+//   // const fail = (e: Error) => sendFailRes(res, e);
+//   // await performSave(user, success, fail);
+// };
 
-const getSingle = async (req: Request, res: Response) => {
-  // const newUser = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const user = new UserModel(newUser);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(user, success, fail);
-};
+// const getSingle = async (req: Request, res: Response) => {
+//   // const newUser = {
+//   //   ...req.body,
+//   //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
+//   // };
+//   // const user = new UserModel(newUser);
+//   // const success = (record: Document) => res.status(200).json(record);
+//   // const fail = (e: Error) => sendFailRes(res, e);
+//   // await performSave(user, success, fail);
+// };
 
-const createUser = async (req: Request, res: Response) => {
-  const newUser = {
-    ...req.body,
-    ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  };
-  const user = new UserModel(newUser);
-  const success = (record: Document) => res.status(200).json(record);
-  const fail = (e: Error) => sendFailRes(res, e);
-  await performSave(user, success, fail);
-};
+// const createUser = async (req: Request, res: Response) => {
+//   const newUser = {
+//     ...req.body,
+//     ownerId: ObjectId.createFromHexString(req.body.ownerId),
+//   };
+//   const user = new UserModel(newUser);
+//   const success = (record: Document) => res.status(200).json(record);
+//   const fail = (e: Error) => sendFailRes(res, e);
+//   await performSave(user, success, fail);
+// };
 
-const updateUser = async (req: Request, res: Response) => {
-  // const newUser = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const user = new UserModel(newUser);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(user, success, fail);
-};
+// const updateUser = async (req: Request, res: Response) => {
+//   // const newUser = {
+//   //   ...req.body,
+//   //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
+//   // };
+//   // const user = new UserModel(newUser);
+//   // const success = (record: Document) => res.status(200).json(record);
+//   // const fail = (e: Error) => sendFailRes(res, e);
+//   // await performSave(user, success, fail);
+// };
 
-const deleteUser = async (req: Request, res: Response) => {
-  // const newUser = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const user = new UserModel(newUser);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(user, success, fail);
-};
+// const deleteUser = async (req: Request, res: Response) => {
+//   // const newUser = {
+//   //   ...req.body,
+//   //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
+//   // };
+//   // const user = new UserModel(newUser);
+//   // const success = (record: Document) => res.status(200).json(record);
+//   // const fail = (e: Error) => sendFailRes(res, e);
+//   // await performSave(user, success, fail);
+// };
 
-export { getAll, getSingle, createUser, updateUser, deleteUser };
+// export { getAll, getSingle, createUser, updateUser, deleteUser };
