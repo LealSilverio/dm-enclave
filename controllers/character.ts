aae24b381b862d36a4a8c7e11536522facd52000
Zach McClure <mcc17067@byui.edu>
2024-07-18 22:28:10 -0600

    Partially Working

--- controllers/character.ts
+++ controllers/character.ts
@@ -15,25 +15,21 @@ const getAll = async (req: Request, res: Response) => {
 };
 
 const getSingle = async (req: Request, res: Response) => {
-  // const newChar = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const character = new CharacterModel(newChar);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(character, success, fail);
+  try {
+    const result = await CharacterModel.findById(ObjectId);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const searchSingle = async (req: Request, res: Response) => {
-  // const newChar = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const character = new CharacterModel(newChar);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(character, success, fail);
+  try {
+    const searchData = await CharacterModel.find(req.query).exec();
+    res.json(searchData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const createCharacter = async (req: Request, res: Response) => {
@@ -48,25 +44,27 @@ const createCharacter = async (req: Request, res: Response) => {
 };
 
 const updateCharacter = async (req: Request, res: Response) => {
-  // const newChar = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const character = new CharacterModel(newChar);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(character, success, fail);
+  try {
+    const updatedData = await CharacterModel.findByIdAndUpdate(req.params.id, req.body, {new: true });
+    if (!updatedData) {
+      return res.status(404).json({ message: 'Data not found' });
+    }
+    res.json(updatedData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 const deleteCharacter = async (req: Request, res: Response) => {
-  // const newChar = {
-  //   ...req.body,
-  //   ownerId: ObjectId.createFromHexString(req.body.ownerId),
-  // };
-  // const character = new CharacterModel(newChar);
-  // const success = (record: Document) => res.status(200).json(record);
-  // const fail = (e: Error) => sendFailRes(res, e);
-  // await performSave(character, success, fail);
+  try {
+    const deletedData = await CharacterModel.findByIdAndDelete(req.params.id);
+    if (!deletedData) {
+      return res.status(404).json({ message: 'Data not found' });
+    }
+    res.json({ message: 'Data deleted successfully' });
+  } catch (error) { 
+    res.status(500).json({ message: error.message });
+  }
 };
 
 export { getAll, getSingle, searchSingle, createCharacter, updateCharacter, deleteCharacter };
--- routes/character.ts
+++ routes/character.ts
@@ -11,8 +11,13 @@ const get1Character = (req: Request, res: Response) => {
   });
 };
 router.get("/1", get1Character);
-router.post("/", createCharacter);
+// router.post("/", createCharacter);
 
 router.get("/", getAll);
+router.get("/", getSingle);
+router.get("/search", searchSingle);
+router.post("/{characterId}", createCharacter);
+router.put("/{characterId}", updateCharacter);
+router.delete("/{characterId}", deleteCharacter);
 
 export default router;
