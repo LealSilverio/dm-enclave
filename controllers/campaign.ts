71efae36fd06550b26727f6f4df849f01bb3f6b2
dallinmoak <dallinmoak@gmail.com>
2024-07-21 00:14:59 -0600

    save point: started work on campaign endpoints

--- controllers/campaign.ts
+++ controllers/campaign.ts
@@ -1,9 +1,9 @@
-// import { ObjectId } from "mongodb";
-// import CampaignModel from "../models/campaign";
-// import { Request, Response } from "express";
-// import performSave from "../db/save";
-// import { sendFailRes } from "./utils";
-// import { Document } from "mongoose";
+import { ObjectId } from "mongodb";
+import CampaignModel from "../models/campaign";
+import { Request, Response } from "express";
+import performSave from "../db/save";
+import { sendFailRes } from "./utils";
+import { Document } from "mongoose";
 
 // const getAll = async (req: Request, res: Response) => {
 //   // const newCamp = {
@@ -27,16 +27,16 @@
 //   // await performSave(campaign, success, fail);
 // };
 
-// const createCampaign = async (req: Request, res: Response) => {
-//   const newCamp = {
-//     ...req.body,
-//     ownerId: ObjectId.createFromHexString(req.body.ownerId),
-//   };
-//   const campaign = new CampaignModel(newCamp);
-//   const success = (record: Document) => res.status(200).json(record);
-//   const fail = (e: Error) => sendFailRes(res, e);
-//   await performSave(campaign, success, fail);
-// };
+const createCampaign = async (req: Request, res: Response) => {
+  console.log("ownerId", req.body.ownerId);
+  const newCamp = req.body;
+  const campaign = new CampaignModel(newCamp);
+  await performSave(
+    campaign,
+    (record) => res.status(200).json(record),
+    (e) => sendFailRes(res, e)
+  );
+};
 
 // const updateCampaign = async (req: Request, res: Response) => {
 //   // const newCamp = {
@@ -60,4 +60,4 @@
 //   // await performSave(campaign, success, fail);
 // };
 
-// export { getAll, getSingle, createCampaign, updateCampaign, deleteCampaign };
+export { createCampaign };
